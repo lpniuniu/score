cd237adbd39f631029f7227387d3591e9efb730f
fanfei <hzfanfei@corp.netease.com>
2016-06-10 16:53:02 +0800

    init project

---
+++ score/score/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  score
+//
+//  Created by FanFamily on 16/6/10.
+//  Copyright © 2016年 niuniu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor.redColor()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
